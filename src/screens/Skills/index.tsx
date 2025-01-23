4bd8457231db7197b8b04e49678fe0fb52c7a1e0
Vitório Corrêa <vitcorreapereira@hotmail.com>
2025-01-23 19:43:31 -0300

    Add Tailwind CSS icon to Skills component

--- src/screens/Skills/index.tsx
+++ src/screens/Skills/index.tsx
@@ -1,4 +1,5 @@
 import { FaReact, FaHtml5, FaCss3Alt, FaJs, FaNodeJs } from "react-icons/fa";
+import { SiTailwindcss } from "react-icons/si";
 
 export const Skills = () => {
     const skills = [
@@ -7,11 +8,11 @@ export const Skills = () => {
         { name: "JavaScript", icon: <FaJs className="h-16 w-16 mb-4 text-yellow-500" /> },
         { name: "React", icon: <FaReact className="h-16 w-16 mb-4 text-blue-500" /> },
         { name: "Node.js", icon: <FaNodeJs className="h-16 w-16 mb-4 text-green-500" /> },
-        
+        { name: "Tailwind CSS", icon: <SiTailwindcss className="h-16 w-16 mb-4 text-teal-500" /> },
     ];
 
     return (
-        <div id="skills" className="bg-black h-screen flex flex-col items-center ">
+        <div id="skills" className="bg-black h-screen flex flex-col items-center">
             <div className="w-full max-w-[1200px] mx-auto p-5">
                 <h1 className="text-white text-4xl font-bold mb-8">Skills</h1>
                 <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-8 mt-20">
